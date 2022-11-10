4e321ca612ea2562b3c739d6533fed0f7ac3636c
Wilmar Javier Becerra Aragon <wjbecerra@utp.edu.co>
2022-11-09 23:03:08 -0500

    Cambio de configuración de paquete de idioma de Quasar a español.

---
+++ src/i18n/en-ES/index.js
@@ -0,0 +1,7 @@
+// Este es solo un ejemplo,
+// para que pueda eliminar de forma segura todos los accesorios predeterminados a continuación
+
+export default {
+  fallido: 'Acción fallida',
+  exitoso: 'Acción exitosa'
+}
--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,5 +1,11 @@
 import enUS from './en-US'
+import enES from './en-ES' 
 
 export default {
   'en-US': enUS
 }
+
+export default {
+  'en-US': enUS
+}
+
