e73224a909d561f2d6b448bef87c5a232cca837b
Daniel Becerra <daniel-3223@hotmail.com>
2023-01-17 09:54:51 -0500

    empresas

--- src/pages/CompanyPage.vue
+++ src/pages/CompanyPage.vue
@@ -1,188 +1,295 @@
 <template>
   <q-page class="q-pa-md q-gutter-sm">
-    <div>
-      <transition appear enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
-        <div>
-          <q-space />
-          <q-table dense :rows-per-page-options="[10, 15, 20, 25, 50, 0]" v-model:pagination="pagination" title="Usuarios" :rows="dataCompany" :filter="filter" :columns="columns" row-key="name" >
-            <template v-slot:top-left>
-              <q-btn unelevated rounded icon="add" color="primary" @click="creating" label="Agregar"/>
-              <q-space />
-            </template>
-            <template v-slot:top-right>
-              <q-input dense debounce="300" v-model="filter" placeholder="Buscar">
-                <template v-slot:append>
-                  <q-icon name="search" />
-                </template>
-              </q-input>
-            </template>
-            <template v-slot:body="props">
-              <q-tr :props="props">
-                <q-td key="name" :props="props">
-                  {{ props.row.name }}
-                </q-td>
-                <q-td key="edit" :props="props">
-                  <q-btn round size="xs" color="primary" icon="border_color" v-on:click="editing(props.row)" />
-                </q-td>
-                <q-td key="delete" :props="props">
-                  <q-btn round size="xs" color="negative" icon="delete_forever" v-on:click="onDelete(props.row)" />
-                </q-td>
-              </q-tr>
-            </template>
-          </q-table>
-        </div>
-      </transition>
-      <q-inner-loading :showing="visible">
-        <q-spinner-pie color="primary" size="70px"/>
-      </q-inner-loading>
-    </div>
+    <q-table
+      dense
+      title="Empresas"
+      :rows="rows"
+      :filter="filter"
+      :columns="columns"
+      row-key="id"
+      :loading="loading"
+      color="primary"
+    >
+      <template v-slot:top-left>
+        <q-btn unelevated rounded icon="add" color="primary" @click="creating" label="Agregar"/>
+      </template>
+      <template v-slot:top-right>
+        <q-input dense debounce="300" v-model="filter" placeholder="Buscar">
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+      <template v-slot:body="props">
+        <q-tr :props="props">
+          <q-td key="name" :props="props">
+            {{ props.row.name }}
+          </q-td>
+          <q-td key="legal_representative" :props="props">
+            {{ props.row.legal_representative }}
+          </q-td>
+          <q-td key="company_type" :props="props">
+            {{ companyTypes.find((company_type) => company_type.id === props.row.company_type).name }}
+          </q-td>
+          <q-td key="email" :props="props">
+            {{ props.row.email }}
+          </q-td>
+          <q-td key="third" :props="props">
+            {{ props.row.third.fullname }}
+          </q-td>
+          <q-td key="license" :props="props">
+            {{ props.row.license }}
+          </q-td>
+          <q-td key="status" :props="props">
+            {{ statuses.find((status) => status.id === props.row.status).name }}
+          </q-td>
+          <q-td key="neighborhood" :props="props">
+            {{ props.row.neighborhood.description }}
+          </q-td>
+          <q-td key="edit" :props="props">
+            <q-btn round size="xs" color="primary" icon="border_color" @click="editing(props.row)" />
+          </q-td>
+          <q-td key="delete" :props="props">
+            <q-btn round size="xs" color="negative" icon="delete_forever" @click="onDelete(props.row)" />
+          </q-td>
+        </q-tr>
+      </template>
+    </q-table>
     <q-dialog v-model="dialog" persistent>
-    <q-card style="width: 700px; max-width: 80vw;">
-      <q-linear-progress :value="10" color="blue" />
-
-      <q-card-section class="row items-center">
-        <div class="text-h6"> </div>
-        <q-space />
-        <q-btn icon="close" flat round dense v-close-popup />
-      </q-card-section>
-
-      <q-banner class="bg-grey-3">
-        <template v-slot:avatar>
-          <q-icon name="warning" color="warning" />
-        </template>
-        Los campos marcados con (*) son obligatorios
-      </q-banner>
-
-      <q-card-section>
-        <q-form ref="myForm" @submit.prevent="">
-          <div class="row justify-around">
-            <div class="col-md-5">
-              <q-input
-                white
-                color="blue"
-                v-model="name"
-                label="Compañía *"
-                lazy-rules
-                :rules="[ val => val && val.length > 0 || 'El campo es obligatorio']"
-              />
+      <q-card style="width: 700px; max-width: 80vw;">
+        <q-card-section class="row items-center">
+          <div class="text-h6">Empresas</div>
+          <q-space />
+          <q-btn icon="close" flat round dense v-close-popup />
+        </q-card-section>
+        <q-banner class="bg-grey-3">
+          <template v-slot:avatar>
+            <q-icon name="warning" color="warning" />
+          </template>
+          Los campos marcados con (*) son obligatorios
+        </q-banner>
+        <q-card-section>
+          <q-form ref="myForm" @submit.prevent="">
+            <div class="row q-col-gutter-sm">
+              <div class="col-6">
+                <q-input filled v-model="company.name" label="Nombre *" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo es obligatorio']"/>
+              </div>
+              <div class="col-6">
+                <q-input filled v-model="company.legal_representative" label="Representante legal *" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo es obligatorio']"/>
+              </div>
+              <div class="col-md-6">
+                <q-select
+                  filled
+                  v-model="company.company_type"
+                  :options="companyTypes"
+                  option-label="name"
+                  option-value="id"
+                  map-options
+                  emit-value
+                  label="Tipo"
+                  lazy-rules
+                  :rules="[ val => val && val > 0 || 'El campo es obligatorio']"
+                />
+              </div>
+              <div class="col-6">
+                <q-input filled v-model="company.email" label="Correo electrónico *" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo es obligatorio']"/>
+              </div>
+              <div class="col-md-6">
+                <q-select
+                  filled
+                  v-model="company.third"
+                  :options="thirds"
+                  option-label="fullname"
+                  option-value="id"
+                  map-options
+                  emit-value
+                  label="Tercero"
+                  lazy-rules
+                  :rules="[ val => val && val > 0 || 'El campo es obligatorio']"
+                />
+              </div>
+              <div class="col-6">
+                <q-input filled v-model="company.license" label="Licencia *" lazy-rules :rules="[ val => val && val.length > 0 || 'El campo es obligatorio']"/>
+              </div>
+              <div class="col-md-6">
+                <q-select
+                  filled
+                  v-model="company.status"
+                  :options="statuses"
+                  option-label="name"
+                  option-value="id"
+                  map-options
+                  emit-value
+                  label="Estado"
+                  lazy-rules
+                  :rules="[ val => val && val > 0 || 'El campo es obligatorio']"
+                />
+              </div>
+              <div class="col-md-6">
+                <q-select
+                  filled
+                  v-model="company.neighborhood"
+                  :options="neighborhoods"
+                  option-label="description"
+                  option-value="code"
+                  map-options
+                  emit-value
+                  label="Barrios"
+                  lazy-rules
+                  :rules="[ val => val && val > 0 || 'El campo es obligatorio']"
+                />
+              </div>
             </div>
-            <div class="col-md-5">
-            </div>
-          </div>
-        </q-form>
-      </q-card-section>
-
-      <div class="row justify-between">
-        <q-card-actions align="left" class="bg-white text-teal">
-        </q-card-actions>
+          </q-form>
+        </q-card-section>
         <q-card-actions align="right" class="bg-white text-teal">
-          <div v-if="!isEditing">
-            <q-btn round icon="save" @click.prevent="onSubmit" color="primary"/>
-            <q-tooltip>Guardar datos</q-tooltip>
-          </div>
-
-          <div v-else>
-            <q-btn round icon="save" @click.prevent="onEditing" color="primary"/>
-            <q-tooltip>Editar datos</q-tooltip>
-          </div> &nbsp;
-
-          <div>
-            <q-btn round icon="cancel" v-close-popup color="negative"/>
-            <q-tooltip>Cancelar</q-tooltip>
-          </div>
+          <q-btn label="Cancelar" v-close-popup color="negative"/>
+          <q-btn :disable="disableSave" v-if="!isEditing" label="Guardar" @click.prevent="onCreate" color="primary"/>
+          <q-btn v-else label="Actualizar" @click.prevent="onEdit" color="primary"/>
         </q-card-actions>
-      </div>
-    </q-card>
-  </q-dialog>
+      </q-card>
+    </q-dialog>
   </q-page>
-  </template>
+</template>
+
 <script>
-import { defineComponent, ref, onMounted } from 'vue'
-import { useQuasar } from 'quasar'
+import { defineComponent, onMounted, reactive, ref } from 'vue'
 import { api } from 'boot/axios'
+import { useQuasar } from 'quasar'
 
 export default defineComponent({
-  name: 'CompanyPage',
+  name: 'companyPage',
   setup () {
-    const path = '/socials'
-    const dialog = ref(false)
-    const visible = ref(false)
-    const id = ref(null)
-    const filter = ref(null)
-    const dataCompany = ref([])
-    const name = ref(null)
-    const myForm = ref(null)
     const $q = useQuasar()
-    const pagination = ref({
-      page: 1,
-      rowsPerPage: 10
+
+    const path = 'companies'
+    const id = ref()
+    const myForm = ref(null)
+    const company = reactive({
+      name: null,
+      legal_representative: null,
+      company_type: null,
+      email: null,
+      third: null,
+      license: null,
+      status: null,
+      neighborhood: null
     })
+    const thirds = ref([])
+    const neighborhoods = ref([])
+    const rows = ref([])
+    const filter = ref('')
+    const columns = [
+      { name: 'name', align: 'center', label: 'Nombre', field: 'name', sortable: true },
+      { name: 'legal_representative', align: 'center', label: 'Representante legal', field: 'legal_representative', sortable: true },
+      { name: 'company_type', align: 'center', label: 'Tipo', field: 'company_type', sortable: true },
+      { name: 'email', align: 'center', label: 'Correo electrónico', field: 'email', sortable: true },
+      { name: 'third', align: 'center', label: 'Tercero', field: 'third', sortable: true },
+      { name: 'license', align: 'center', label: 'Licencia', field: 'license', sortable: true },
+      { name: 'status', align: 'center', label: 'Estado', field: 'status', sortable: true },
+      { name: 'neighborhood', align: 'center', label: 'Barrio', field: 'neighborhood', sortable: true },
+      { name: 'edit', align: 'center', label: 'Editar', field: 'edit', sortable: false },
+      { name: 'delete', align: 'center', label: 'Eliminar', field: 'delete', sortable: false }
+    ]
+    const companyTypes = [
+      { id: 0, name: 'Ninguno' },
+      { id: 1, name: 'Público' },
+      { id: 2, name: 'Privado' }
+    ]
+    const statuses = [
+      { id: 1, name: 'Activo' },
+      { id: 2, name: 'Inactivo' }
+    ]
+    const loading = ref(false)
+    const dialog = ref(false)
     const isEditing = ref(false)
-    const columns = ref([
-      { name: 'name', align: 'center', label: 'Compañía', field: 'name', sortable: true },
-      { name: 'edit', align: 'center', label: 'Editar', field: 'edit', sortable: true },
-      { name: 'delete', align: 'center', label: 'Eliminar', field: 'delete', sortable: true }
-    ])
+    const disableSave = ref(false)
 
-    onMounted(() => {
-      getCompanies()
-    })
-
-    const getCompanies = async () => {
-      visible.value = true
-      const { data } = await api.get(path)
-      dataCompany.value = data
-      visible.value = false
+    const getAll = async () => {
+      loading.value = true
+      api.get(path).then(response => {
+        rows.value = response.data
+      }).catch(e => {
+        if (e) {
+          console.log(e)
+        }
+      }).finally(() => {
+        loading.value = false
+      })
     }
-
+    const getThirds = async () => {
+      api.get('thirdperson').then(response => {
+        thirds.value = response.data
+      }).catch(e => {
+        if (e) {
+          console.log(e)
+        }
+      }).finally(() => {
+        loading.value = false
+      })
+    }
+    const getNeihborhoods = async () => {
+      api.get('neighborhoods').then(response => {
+        neighborhoods.value = response.data
+      }).catch(e => {
+        if (e) {
+          console.log(e)
+        }
+      }).finally(() => {
+        loading.value = false
+      })
+    }
     const creating = () => {
       onReset()
+      isEditing.value = false
       dialog.value = true
     }
-
-    const onReset = () => {
-      name.value = null
-      isEditing.value = false
-    }
-
-    const onSubmit = () => {
-      myForm.value.validate().then(async success => {
+    const onCreate = () => {
+      myForm.value.validate().then(success => {
         if (success) {
-          api.post(path, {
-            name: name.value
-          }).then(() => {
+          api.post(path, company).then(response => {
+            getAll()
             dialog.value = false
-            getCompanies()
+          }).catch(e => {
+            if (e) {
+              console.log(e)
+            }
           })
         }
       })
     }
-
     const editing = (row) => {
       onReset()
-      dialog.value = true
-      isEditing.value = true
       id.value = row.id
-      name.value = row.name
+      company.name = row.name
+      company.legal_representative = row.legal_representative
+      company.company_type = row.company_type
+      company.email = row.email
+      company.third = row.third.id
+      company.license = row.license
+      company.neighborhood = row.neighborhood.code
+      company.status = row.status
+      isEditing.value = true
+      dialog.value = true
     }
-
-    const onEditing = () => {
-      myForm.value.validate().then(async success => {
+    const onEdit = () => {
+      myForm.value.validate().then(success => {
         if (success) {
-          api.patch(path + '/' + id.value, {
-            name: name.value
-          }).then(() => {
+          api.patch(`${path}/${id.value}`, company).then(response => {
+            getAll()
             dialog.value = false
-            getCompanies()
+          }).catch(e => {
+            if (e) {
+              console.log(e)
+            }
           })
         }
       })
     }
-
     const onDelete = (row) => {
       $q.dialog({
         title: 'Confirmación',
-        message: '¿Está seguro que desea eliminar este registro: ' + row.name + '?',
+        message: 'Desea eliminar el registro?',
         ok: {
           label: 'Si',
           color: 'positive'
@@ -192,29 +299,41 @@ export default defineComponent({
           color: 'negative'
         }
       }).onOk(() => {
-        api.delete(path + '/' + row.id).then(response => {
-          dialog.value = false
-          getCompanies()
+        api.delete(`${path}/${row.id}`).then(response => {
+          getAll()
+        }).catch(e => {
+          if (e) {
+            console.log(e)
+          }
         })
       })
     }
+    const onReset = () => Object.keys(company).forEach((i) => (company[i] = null))
+
+    onMounted(async () => {
+      getAll()
+      getThirds()
+      getNeihborhoods()
+    })
 
     return {
-      dialog,
-      dataCompany,
-      isEditing,
-      name,
+      company,
       myForm,
-      pagination,
-      creating,
+      rows,
       columns,
-      visible,
+      companyTypes,
+      statuses,
+      thirds,
+      neighborhoods,
       filter,
-      onReset,
-      onSubmit,
+      loading,
+      dialog,
+      isEditing,
+      disableSave,
+      creating,
+      onCreate,
       editing,
-      onEditing,
-      id,
+      onEdit,
       onDelete
     }
   }
